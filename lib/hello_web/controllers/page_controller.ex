d109bafbcf4da5ddc38842cb05c3a833a7c44757
Tim Littlemore <tim@tjl2.com>
2022-01-28 22:12:23 +0000

    Redirects

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -1,9 +1,9 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, :index)
-  end
+  # def index(conn, _params) do
+  #   render(conn, :index)
+  # end
 
   # Example to send custom response
   # def index(conn, _params) do
@@ -11,4 +11,19 @@ defmodule HelloWeb.PageController do
   #   |> put_resp_content_type("text/plain")
   #   |> send_resp(201, "")
   # end
+
+  # Notice that the redirect function takes conn as well as a string
+  # representing a relative path within our application. For security reasons,
+  # the :to helper can only redirect for paths within your application. If you
+  # want to redirect to a fully-qualified path or an external URL, you should
+  # use :external instead:
+  # redirect(conn, external: "https://elixir-lang.org/")
+  def index(conn, _params) do
+    redirect(conn, to: Routes.page_path(conn, :redirect_test))
+  end
+
+  def redirect_test(conn, _params) do
+    render(conn, :index)
+  end
+
 end
--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -19,6 +19,7 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/redirect_test", PageController, :redirect_test
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
 
