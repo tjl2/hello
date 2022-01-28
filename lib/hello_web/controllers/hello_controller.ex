163971e987d2104da33b8532ed93ae09b23ef433
Tim Littlemore <tim@tjl2.com>
2022-01-28 20:16:13 +0000

    Add /hello/:messenger route, action, template

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -4,4 +4,12 @@ defmodule HelloWeb.HelloController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  # If the body of the action needs access to the full map of parameters bound
+  # to the params variable, in addition to the bound messenger variable, we
+  # could define show/2 like this:
+  # def show(conn, %{"messenger" => messenger} = params) do ...
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, "show.html", messenger: messenger)
+  end
 end
