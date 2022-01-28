53f2521df41e34e93fada3a7948c5f76d878d308
Tim Littlemore <tim@tjl2.com>
2022-01-28 22:01:38 +0000

    Sending responses directly
    If none of the rendering options above quite fits our needs, we can compose our own using some of the functions that Plug gives us. Let's say we want to send a response with a status of "201" and no body whatsoever. We can do that with the Plug.Conn.send_resp/3 function.

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -4,4 +4,11 @@ defmodule HelloWeb.PageController do
   def index(conn, _params) do
     render(conn, :index)
   end
+
+  # Example to send custom response
+  # def index(conn, _params) do
+  #   conn
+  #   |> put_resp_content_type("text/plain")
+  #   |> send_resp(201, "")
+  # end
 end
