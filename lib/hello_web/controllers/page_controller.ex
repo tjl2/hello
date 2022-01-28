7b92c3b2d5e2cd2e8630fc613ec758107f5a1564
Tim Littlemore <tim@tjl2.com>
2022-01-28 21:58:08 +0000

    http://localhost:4000/?_format=text

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    conn
-    |> put_root_layout("admin.html")
-    |> render("index.html")
+    render(conn, :index)
   end
 end
