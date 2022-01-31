76dfa467df764cabb372b1ce155923848975b906
Tim Littlemore <tim@tjl2.com>
2022-01-31 15:23:57 +0000

    Make use of render_one and render_many

--- lib/hello_web/views/page_view.ex
+++ lib/hello_web/views/page_view.ex
@@ -6,10 +6,14 @@ defmodule HelloWeb.PageView do
   # end
 
   def render("index.json", %{pages: pages}) do
-    %{data: Enum.map(pages, fn page -> %{title: page.title} end)}
+    %{data: render_many(pages, HelloWeb.PageView, "page.json")}
   end
 
   def render("show.json", %{page: page}) do
-    %{data: %{title: page.title}}
+    %{data: render_one(page, HelloWeb.PageView, "page.json")}
+  end
+
+  def render("page.json", %{page: page}) do
+    %{title: page.title}
   end
 end
