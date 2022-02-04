8c5826b152635e103ed2e4c81caf0eb2e81f0aa7
Tim Littlemore <tim@tjl2.com>
2022-02-04 16:57:57 +0000

    Associate products to categories in the schema

--- lib/hello/catalog/product.ex
+++ lib/hello/catalog/product.ex
@@ -8,6 +8,13 @@ defmodule Hello.Catalog.Product do
     field :title, :string
     field :views, :integer
 
+    # We use Ecto.Schema's many_to_many macro to let Ecto know how to associate
+    # our product to multiple categories through the "product_categories" join
+    # table. We also use the on_replace: :delete option to declare that any
+    # existing join records should be deleted when we are changing our
+    # categories.
+    many_to_many :categories, Category, join_through: "product_categories", on_replace: :delete
+
     timestamps()
   end
 
