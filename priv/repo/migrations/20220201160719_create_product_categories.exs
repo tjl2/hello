b9bf2c35e77bb9b3bc3bf2eae0223d043f41e901
Tim Littlemore <tim@tjl2.com>
2022-02-04 16:51:54 +0000

    Description of the migration

--- priv/repo/migrations/20220201160719_create_product_categories.exs
+++ priv/repo/migrations/20220201160719_create_product_categories.exs
@@ -1,3 +1,11 @@
+# We created a product_categories table and used the primary_key: false option
+# since our join table does not need a primary key. Next we defined our
+# :product_id and :category_id foreign key fields, and passed on_delete:
+# :delete_all to ensure the database prunes our join table records if a linked
+# product or category is deleted. By using a database constraint, we enforce
+# data integrity at the database level, rather than relying on ad-hoc and
+# error-prone application logic. Next, we created indexes for our foreign keys,
+# and a unique index to ensure a product cannot have duplicate categories.
 defmodule Hello.Repo.Migrations.CreateProductCategories do
   use Ecto.Migration
 
