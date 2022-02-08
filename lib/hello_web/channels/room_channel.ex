45d802db3132b44a4b8bd2925206c4aa6b066608
Tim Littlemore <tim@tjl2.com>
2022-02-08 17:06:26 +0000

    Live updating messages

--- lib/hello_web/channels/room_channel.ex
+++ lib/hello_web/channels/room_channel.ex
@@ -12,4 +12,9 @@ defmodule HelloWeb.RoomChannel do
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "Unauthorized"}}
   end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast!(socket, "new_msg", %{body: body})
+    {:noreply, socket}
+  end
 end
